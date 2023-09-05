1b14a3df3fbd6fd1ecf85d9a07fb4876a5213c06
HEEKWON <higun295@gmail.com>
2023-09-05 23:24:15 +0900

    20230905_4

---
+++ WpfExplorer.Support/UI/Units/DarkWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfExplorer.Support.UI.Units
+{
+    public class DarkWindow : Control
+    {
+        static DarkWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DarkWindow), new FrameworkPropertyMetadata(typeof(DarkWindow)));
+        }
+    }
+}
