101f8d169c19e5376acf17db53434bae14a55368
HEEKWON <higun295@gmail.com>
2023-09-16 14:16:05 +0900

    add left margin for children treeitems

--- WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
+++ WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
@@ -1,27 +1,28 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using Jamesnet.Wpf.Mvvm;
 using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Input;
 using WpfExplorer.Support.Local.Helpers;
 using WpfExplorer.Support.Local.Models;
 
 namespace WpfExplorer.Main.Local.ViewModels
 {
-    public class MainContentViewModel
+    public partial class MainContentViewModel : ObservableBase
     {
-        public ICommand FolderChangedCommand { get; init; }
+        private readonly FileService _fileService;
 
         public List<FolderInfo> Roots { get; init; }
 
         public MainContentViewModel(FileService fileService)
         {
-            FolderChangedCommand = new RelayCommand<FolderInfo>(FolderChanged);
+            _fileService = fileService;
+
             Roots = fileService.GenerateRootNodes();
         }
 
+        [RelayCommand]
         private void FolderChanged(FolderInfo folderInfo)
         {
-            MessageBox.Show($"Selected : {folderInfo.Name}");
+            _fileService.RefreshSubdirectories(folderInfo);
         }
     }
 }
