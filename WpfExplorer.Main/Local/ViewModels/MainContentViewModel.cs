8ba0b4c5d6f802dd2feb4ec6d388a09bd14d97e0
HEEKWON <higun295@gmail.com>
2023-09-16 13:31:05 +0900

    add TreeViewItem Changed Event

--- WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
+++ WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
 using WpfExplorer.Support.Local.Helpers;
 using WpfExplorer.Support.Local.Models;
 
@@ -6,11 +9,19 @@ namespace WpfExplorer.Main.Local.ViewModels
 {
     public class MainContentViewModel
     {
+        public ICommand FolderChangedCommand { get; init; }
+
         public List<FolderInfo> Roots { get; init; }
 
         public MainContentViewModel(FileService fileService)
         {
+            FolderChangedCommand = new RelayCommand<FolderInfo>(FolderChanged);
             Roots = fileService.GenerateRootNodes();
+        }
+
+        private void FolderChanged(FolderInfo folderInfo)
+        {
+            MessageBox.Show($"Selected : {folderInfo.Name}");
         }
     }
 }
