0049d3c333df9697a270a3902abcfac5e09c1468
HEEKWON <higun295@gmail.com>
2023-09-17 23:31:28 +0900

    use memento design pattern

---
+++ WpfExplorer.Support/Local/Models/LocationChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WpfExplorer.Support.Local.Models
+{
+    public class LocationChangedEventArgs : EventArgs
+    {
+        public FileInfoBase Current { get; }
+        public LocationChangedEventArgs(FileInfoBase current)
+        {
+            Current = current;
+        }
+    }
+}
