00cbc77d9383e48e20ada56af5f883882732dbc7
HEEKWON <higun295@gmail.com>
2023-09-19 22:46:16 +0900

    20230919_1

--- WpfExplorer.Support/Local/Helpers/NavigatorService.cs
+++ WpfExplorer.Support/Local/Helpers/NavigatorService.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WpfExplorer.Support.Local.Models;
 
 namespace WpfExplorer.Support.Local.Helpers
