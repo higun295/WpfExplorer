61cf3f265a6fbb47ae8752f18aaba8870305cbef
HEEKWON <higun295@gmail.com>
2023-09-19 22:53:00 +0900

    20230919_2

--- WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
+++ WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
@@ -9,20 +9,28 @@ namespace WpfExplorer.Main.Local.ViewModels
     public partial class MainContentViewModel : ObservableBase
     {
         private readonly FileService _fileService;
+        private readonly NavigatorService _navigatorService;
 
         public List<FolderInfo> Roots { get; init; }
 
-        public MainContentViewModel(FileService fileService)
+        public MainContentViewModel(FileService fileService, NavigatorService navigatorService)
         {
             _fileService = fileService;
+            _navigatorService = navigatorService;
+            _navigatorService.LocationChanged += _navigatorService_LocationChanged;
 
             Roots = fileService.GenerateRootNodes();
+        }
+
+        private void _navigatorService_LocationChanged(object? sender, LocationChangedEventArgs e)
+        {
         }
 
         [RelayCommand]
         private void FolderChanged(FolderInfo folderInfo)
         {
             _fileService.RefreshSubdirectories(folderInfo);
+            _navigatorService.ChangeLocation(folderInfo);
         }
     }
 }
