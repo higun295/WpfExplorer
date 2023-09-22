4ccf14da5de6b02714329e5f2b8d9c0060826221
HEEKWON <higun295@gmail.com>
2023-09-22 18:01:15 +0900

    add LocationContent

---
+++ WpfExplorer.Location/Local/ViewModels/LocationContentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Jamesnet.Wpf.Mvvm;
+using WpfExplorer.Support.Local.Helpers;
+
+namespace WpfExplorer.Location.Local.ViewModels
+{
+    public partial class LocationContentViewModel : ObservableBase
+    {
+        private readonly NavigatorService _navigatorService;
+
+        public LocationContentViewModel(NavigatorService navigatorService)
+        {
+            _navigatorService = navigatorService;
+        }
+    }
+}
